0ae53e8117349f938c613e50454c095ca257cd74
Emir Dedic <dedic.emir17@gmail.com>
2022-04-14 19:10:20 +0200

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,14 +1,16 @@
 const MY_CAR = {
     wheels: 4,
     cylinders: 4,
-    size: 2.2
-};
-
-function main(myCar) {
-    const { cylinders, size } = myCar;
-    return { cylinders, size };
-}
-
-console.log(main(MY_CAR));
-
-module.exports = main;
+    size: 2.2,
+  };
+  function main(myCar) {
+    // Only change code below this line
+  
+    const { cylinders: carCylinders, size: carSize } = MY_CAR;
+    return { carCylinders, carSize };
+  
+    // Only change code above this line
+  }
+  
+  console.log(main(MY_CAR));
+  module.exports = main;
